c9a2ad88270eb5b6abc702f359b7020888aef050
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-07-07 17:52:14 +0100

    wip

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,26 @@
-const allowedOrigins = require('./allowedOrigins')
+// const allowedOrigins = require('./allowedOrigins')
+
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//    },
+//    credentials: true,
+//    optionsSuccessStatus: 200
+// }
+
+// module.exports = corsOptions
+
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-   },
-   credentials: true,
-   optionsSuccessStatus: 200
-}
+  origin: 'https://qso-frontend.vercel.app', // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
+  optionsSuccessStatus: 200 // For legacy browser support
+};
 
-module.exports = corsOptions
+module.exports = corsOptions;
--- server.js
+++ server.js
@@ -18,26 +18,7 @@ connectDB()
 
 app.use(logger)
 
-// app.use(cors(corsOptions))
-
-
-app.use(cors({
-  origin: 'https://qso-frontend.vercel.app', // Allow this origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allow these methods
-  allowedHeaders: 'Content-Type,Authorization', // Allow these headers
-  optionsSuccessStatus: 200 // For legacy browser support
-}));
-
-// Handle preflight requests
-app.options('*', cors({
-  origin: 'https://qso-frontend.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization',
-  optionsSuccessStatus: 200
-}));
-
-
-
+app.use(cors(corsOptions))
 
 app.use(express.urlencoded({ extended: false }))
 
