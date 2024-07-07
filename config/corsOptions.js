08c055342bad7a40bc8399e2ec3bb92cd69293c4
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-07-07 17:58:52 +0100

    wip

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,26 +1,15 @@
-// const allowedOrigins = require('./allowedOrigins')
-
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//    },
-//    credentials: true,
-//    optionsSuccessStatus: 200
-// }
-
-// module.exports = corsOptions
-
+const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
-  origin: 'https://qso-frontend.vercel.app', // Replace with your frontend URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true,
-  optionsSuccessStatus: 200 // For legacy browser support
-};
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+   },
+   credentials: true,
+   optionsSuccessStatus: 200
+}
 
-module.exports = corsOptions;
+module.exports = corsOptions
