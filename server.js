1591420d4bb21b681843b66ffc9c89f325f17979
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-07-07 17:45:18 +0100

    wip

--- server.js
+++ server.js
@@ -18,13 +18,26 @@ connectDB()
 
 app.use(logger)
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.use(express.urlencoded({ extended: false }))
 
 app.use(express.json())
 
 app.use(cookieParser())
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
