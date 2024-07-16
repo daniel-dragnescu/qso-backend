3e462b3cf3cb0051944fed4156fc2a533ca4b1b6
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-07-16 14:47:35 +0300

    update

--- api/auth/register.js
+++ api/auth/register.js
@@ -2,20 +2,17 @@ const { handleRegister } = require('../../controllers/registerController');
 const connectDB = require('../../config/dbConn');
 const allowCors = require('../../controllers/allowCors'); // Assuming allowCors is middleware
 
-module.exports = allowCors(async (req, res) => {
-  await connectDB();
-
-  // Handle OPTIONS method
-  if (req.method === 'OPTIONS') {
-    res.status(204).end();
-    return;
-  }
-
-  // Handle POST method
-  if (req.method === 'POST') {
-    handleRegister(req, res);
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-});
+const handler = async (req, res) => {
+    // Connect to the database
+    await connectDB();
+  
+    if (req.method === 'POST') {
+      return handleRegister(req, res);
+    } else if (req.method === 'OPTIONS') {
+      return res.status(204).end();
+    } else {
+      return res.status(405).json({ message: 'Method Not Allowed' });
+    }
+  };
+  
+  module.exports = allowCors(handler);
